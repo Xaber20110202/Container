76ebab9690220546d2523e1f582c3a51c25d58bc
Xaber <ruixiang.zen@husor.com.cn>
2015-03-23 16:07:54 +0800

    modify

--- isSupportCss3.js
+++ isSupportCss3.js
@@ -2,7 +2,7 @@ var isSupportCss3 = function (name) {
     var reg = new RegExp(name, 'ig'),
         getComputedStyle = window.getComputedStyle;
 
-    return !!getComputedStyle && _.some( getComputedStyle( $('div')[0], null), function (v) {
+    return !!getComputedStyle && _.some( getComputedStyle( document.body, null ), function (v) {
         return reg.test(v);
     });
 };
