678bd8b1a7fe9dff9cf5746e68596043a2d1c899
Xaber <ruixiang.zen@husor.com.cn>
2015-03-23 16:09:59 +0800

    modify

--- 助你登顶github大神.js
+++ 助你登顶github大神.js
@@ -1,6 +1,3 @@
-
-var colors = ['#eee', '#d6e685', '#8cc665', '#44a340', '#1e6823'];
-
 var randomFunc = function (min, max) {
 	if (max == null) {
       max = min;
@@ -10,10 +7,12 @@ var randomFunc = function (min, max) {
     return min + Math.floor( Math.random() * ( max - min + 1 ) );
 };
 
-var getColor = function (colors, randomFunc) {
-	return colors[ randomFunc( colors.length - 1) ];
+var getRandomItem = function (items, randomFunc) {
+	return items[ randomFunc( items.length - 1) ];
 };
 
+var colors = ['#eee', '#d6e685', '#8cc665', '#44a340', '#1e6823'];
+
 $('rect').each(function () {
-	$(this).attr( 'fill', getColor( colors, randomFunc ) );
+	$(this).attr( 'fill', getRandomItem( colors, randomFunc ) );
 });
