679157daf2554b2984b6248da54a0c3b88ecd8e4
Xaber <ruixiang.zen@husor.com.cn>
2015-06-09 21:10:20 +0800

    更改为 seajs 写法

--- JavaScript/2015.05.01-validate.js
+++ JavaScript/2015.05.01-validate.js
@@ -1,5 +1,4 @@
-(function (win, X) {
-
+define(function (require, exports, module) {
     var toString = Object.prototype.toString;
     var isString = function (obj) {
         return toString.call(obj) === '[object String]';
@@ -7,7 +6,7 @@
 
     var forEach = function (obj, func) {
         for (var key in obj) {
-            if (func) {
+            if (obj.hasOwnProperty(key) && func) {
                 func.call(obj, obj[key], key, obj);
             }
         }
@@ -17,6 +16,7 @@
      *          以防验证规则不一致 各处代码随意编写导致的混乱情况
      * @author  Xaber
      * @rely    nothing
+     * @see     https://github.com/Xaber20110202/Container/blob/master/JavaScript/2015.05.01-validate.js
      */
     var validateConfig = {
         rEmail : /^(?:[a-z\d]+[_\-\+\.]?)*[a-z\d]+@(?:([a-z\d]+\-?)*[a-z\d]+\.)+([a-z]{2,})+$/i,
@@ -55,7 +55,5 @@
         return isString(str) && str.length === length;
     };
 
-    X.validate = validate;
-    return validate;
-
-})(window, window.X || (window.X = {}));
+    module.exports = validate;
+});
